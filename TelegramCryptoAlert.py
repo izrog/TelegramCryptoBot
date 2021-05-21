ba8972eb5a1ab42a548bc9fee0a386ccb71ea9f0
Isrrael <isrraelogz@gmail.com>
2021-05-20 22:50:23 -0500

    new changes

---
+++ TelegramCryptoAlert.py
@@ -0,0 +1,32 @@
+import os
+import schedule
+import time
+import telebot
+import krakenex
+from pykrakenapi import KrakenAPI
+
+API_KEY = os.getenv('API_KEY')
+CHAT_ID = os.getenv('CHAT_ID')
+
+bot = telebot.TeleBot("API_KEY")
+api = krakenex.API()
+k = KrakenAPI(api)
+chatID = CHAT_ID
+coins = ["BTC", "ETH", "DOGE", "ADA", "XRP"]
+
+def price(pair):
+    val = k.get_ticker_information(pair)
+    return "{}: ${}".format(pair, val.a[0][0])
+
+def price_alert():
+    text = ""
+    for coin in coins:
+        pair = coin + "USD"
+        text = text + price(pair) + "\n"
+    bot.send_message(chatID, text)
+
+schedule.every(5).minutes.do(price_alert)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
